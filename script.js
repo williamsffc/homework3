6e0bc667b862f85d714b2431bc2fa4a5e140f5ae
Williams <williamsffc@gmail.com>
2020-07-28 21:34:03 -0700

    added some pseudo coding

--- script.js
+++ script.js
@@ -11,46 +11,43 @@ var workShopPass = [];
 var randomPass = [];
 var password = [];
 
+// ask user how many characters the password should be
 function promptUser() {
 
   var userClicked = prompt("What would the length of your secure password be?(Choose between 8 and 128 Character)");
-
+  // let user pick what type of characters the password should contain
   if (userClicked >= 8 && userClicked < 129) {
     if (confirm("Do you want to include 'UPPERCASE' characters in your password?") === true) {
-      // workShopPass.push(upperCasePass);
       for (var i = 0; i < upperCasePass.length; i++) {
         workShopPass.push(upperCasePass[i])
       }
     }
     if (confirm("Do you want to include 'lowercase' characters in your password?") === true) {
-      // workShopPass.push(lowerCasePass);
       for (var i = 0; i < lowerCasePass.length; i++) {
         workShopPass.push(lowerCasePass[i])
       }
     }
     if (confirm("Do you want to include 'numbers (0,1,...,9)' in your password?") == true) {
-      // workShopPass.push(numbersPass);
       for (var i = 0; i < numbersPass.length; i++) {
         workShopPass.push(numbersPass[i])
       }
     }
     if (confirm("Do you want to include 'Speci@l ch@r@cters' in your password?") === true) {
-      // workShopPass.push(specialCharPass);
       for (var i = 0; i < specialCharPass.length; i++) {
         workShopPass.push(specialCharPass[i])
       }
     }
     console.log(workShopPass);
-
+  // alert user the number entered is below the requirements 
   } else {
     alert('The password must be between 8 and 128 characters long. Try Again!');
   }
-  
+  // from users response, select randomly the amount of characters requested by user
   for (var i = 0; i < userClicked; i++) {
     var randomPass = workShopPass[Math.floor(Math.random() * workShopPass.length)];
     password.push(randomPass);
   }
-
+  // pass that random characters to the screen as a form of text
   document.getElementById("password").innerHTML = password.join("");
 
   console.log(password);
